8c732bdb061f1d95713f6117c835cc70bb504249
Terence <terencejkruger@gmail.com>
2019-12-14 12:17:14 +1030

    Add id service to context

--- context/index-copy.js
+++ context/index-copy.js
@@ -1,19 +1,9 @@
-const $id = require('./id');
-const Mongo = require('./mongo');
 const Image = require('./Image');
-const $logger = require('./logger');
 const Security = require('./security');
 const Doc = require('./Doc');
-const loadModules = require('./modules/loadModules');
-
-const $mongo = Mongo({ $logger });
 
 module.exports = {
-  $id,
-  $logger,
-  $mongo,
   $image: Image({ $logger, $mongo }),
   $doc: Doc(),
   $security: Security({ $id, $mongo }),
-  $modules: loadModules(['modules/**/*.js']),
 };
--- context/index.js
+++ context/index.js
@@ -1,9 +1,11 @@
 const $logger = require('./logger');
 const loadModules = require('./modules/loadModules');
 const Mongo = require('./mongo');
+const $id = require('./id');
 
 module.exports = {
   $logger,
   $modules: loadModules(['modules/**/*.js']),
   $mongo: Mongo({ $logger }),
+  $id,
 };
