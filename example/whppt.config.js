dbf8f6955191385a50414d740739dfae0739ba33
lucas <lvcasjm@gmail.com>
2020-01-30 10:22:32 +1030

    updates to save page etc.

--- example/whppt.config.js
+++ example/whppt.config.js
@@ -2,10 +2,13 @@ module.exports = {
   security: {
     provider: 'jwt',
     jwt: {
-      secret: 'W5rX8JlYWRlclJrLCJqdGkiOiJiaGdkr3bGl6amZ5IiwiYWxnIjoiSFMyNTYiLCP', // use a 256bit key
+      secret: process.env.JWT_SECRET || 'W5rX8JlYWRlclJrLCJqdGkiOiJiaGdkr3bGl6amZ5IiwiYWxnIjoiSFMyNTYiLCP', // use a 256bit key
       issuer: 'example',
       audience: 'example',
     },
   },
-  collections: { pages: 'pages' },
+  collections: {
+    pages: 'pages',
+    listing: 'listing',
+  },
 };
--- modules/page/save.js
+++ modules/page/save.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
+const { collections } = require(process.cwd() + '/whppt.config.js');
 
 module.exports = {
-  exec({ $id, $mongo }, { page }) {
+  exec({ $id, $mongo: { $save } }, { page, collection }) {
+    const _collection = collections[collection];
+    assert(_collection, `Collection ${collection} does not exist.`);
     assert(page, 'A Page Object must be provided.');
 
     page._id = page._id || $id();
-
-    return $mongo.then(({ $db, $save }) => {
-      return $save('pages', page).then(() => page);
-    });
+    return $save('pages', page).then(() => page);
   },
 };
