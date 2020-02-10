d92a7630bb4d69c58fd84ae527c7e43cc12bc1e0
lucas <lvcasjm@gmail.com>
2020-02-10 16:34:21 +1030

    working on atdw categories

--- modules/atdw/fetchListingData.js
+++ modules/atdw/fetchListingData.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { uniqBy } = require('lodash');
+const { map, get, forEach, find, uniqBy } = require('lodash');
 const URI = require('uri-js');
 
 // Whppt Config
@@ -7,6 +7,36 @@ const { atdw } = require(`${process.cwd()}/whppt.config.js`);
 
 module.exports = {
   exec({ $atdw, $mongo: { $db } }, query) {
+    const stringFromPath = function(product, path) {
+      return get(product, path);
+    };
+
+    const atdwFields = {
+      name: stringFromPath,
+      description: stringFromPath,
+      status: stringFromPath,
+      email: function(product) {
+        return find(product.communication, comm => comm.attributeIdCommunication === 'CAEMENQUIR');
+      },
+      physicalAddress: function(product) {
+        return find(product.addresses, address => address.address_type === 'PHYSICAL');
+      },
+      postalAddress: function(product) {
+        return find(product.addresses, address => address.address_type === 'POSTAL');
+      },
+      image: function(product) {
+        const { scheme, host, path } = URI.parse(product.productImage);
+        return `${scheme}://${host}${path}`;
+      },
+      taggedCategories: function(product) {
+        const tags = map(product.verticalClassifications, category => category.productTypeId);
+        tags.push(product.productCategoryId);
+
+        // should uniq this...
+        return tags;
+      },
+    };
+
     const { id } = query;
     assert(id, 'Please provide a Listing ID');
 
@@ -15,30 +45,29 @@ module.exports = {
     assert(apiUrl, 'Please provide an ATDW URL.');
     assert(apiKey, 'Please provide an ATDW API Key.');
 
-    return $atdw
-      .$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${id}`)
-      .then(listingData => {
+    return Promise.all([$atdw.$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${id}`), $db.collection('listings').findOne({ _id: id })]).then(
+      ([listingData, listing]) => {
         const { multimedia } = listingData;
         listingData.multimedia = filterMultimedia(multimedia);
 
+        forEach(atdwFields, (getFieldValue, fieldKey) => {
+          const property = listing[fieldKey];
+
+          if (!property || property.provider !== 'atdw') return;
+          property.value = getFieldValue(listingData, property.path);
+        });
+
+        listing.atdw = { ...listing.atdw, ...listingData };
+        listing.hasFullATDWData = true;
+
         return $db
           .collection('listings')
-          .findOne({ _id: id })
-          .then(({ atdw }) => {
-            const updatedListing = { ...atdw, ...listingData };
-
-            return $db
-              .collection('listings')
-              .updateOne({ _id: id }, { $set: { atdw: updatedListing, hasFullATDWData: true } })
-              .then(() => {
-                return Promise.resolve({ statusCode: 200, message: 'OK' });
-              });
+          .updateOne({ _id: id }, { $set: listing })
+          .then(() => {
+            return Promise.resolve({ statusCode: 200, message: 'OK' });
           });
-      })
-      .catch(err => {
-        console.error(err);
-        throw new Error(`Unable to update data from ATDW for product ${id}`);
-      });
+      }
+    );
   },
 };
 
