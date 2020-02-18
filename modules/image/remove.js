438f0108a35698fc237fa46b51961aade26a4d5e
Ethan <sunnysun9312@gmail.com>
2020-02-18 17:33:22 +1030

    Update remove.js

--- modules/image/remove.js
+++ modules/image/remove.js
@@ -2,6 +2,7 @@ module.exports = {
   exec({ $mongo: { $db }, $image }, { id }) {
     //Not really removing image for now
     console.log('TCL: exec -> id', id);
+    return Promise.resolve();
     // return $image.remove(imageId)
   },
 };
