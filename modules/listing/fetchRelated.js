2bfc2667a21861ace59a071db9c2de0b66defd75
Jake Vandborg <jakevandborg@Jakes-MacBook-Pro.local>
2020-02-21 13:48:19 +1030

    added fallback for no related listing results

--- modules/listing/fetchRelated.js
+++ modules/listing/fetchRelated.js
@@ -4,9 +4,11 @@ module.exports = {
   exec({ $mongo: { $db } }, query) {
     const { categories, _id, limit = 6 } = query;
 
+    const idQuery = {};
+    idQuery['_id'] = {};
+    idQuery['_id'].$ne = _id;
+
     const categoriesQuery = {};
-    categoriesQuery['_id'] = {};
-    categoriesQuery['_id'].$ne = _id;
     categoriesQuery['taggedCategories.value'] = {};
     categoriesQuery['taggedCategories.value'].$in = map(categories, c => {
       return toUpper(c.trim());
@@ -19,9 +21,23 @@ module.exports = {
 
     return $db
       .collection('listings')
-      .find({ ...categoriesQuery, ...toursQuery, ...parentQuery })
+      .find({ ...categoriesQuery, ...toursQuery, ...parentQuery, ...idQuery })
       .toArray()
       .then(listings => {
+        if (!listings.length || !listings.length) {
+          return $db
+            .collection('listings')
+            .find({ ...idQuery, ...parentQuery, ...toursQuery })
+            .toArray()
+            .then(listings => {
+              if (!listings || !listings.length) return { listings: [] };
+              return { listings: shuffle(listings).slice(0, limit) };
+            })
+            .catch(err => {
+              console.error(err);
+              throw err;
+            });
+        }
         return { listings: shuffle(listings).slice(0, limit) };
       })
       .catch(err => {
