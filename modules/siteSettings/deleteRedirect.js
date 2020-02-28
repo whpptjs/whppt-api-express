c92025667995bded1d8a4ffb0b6d00dd11430565
Jake Vandborg <jakevandborg@Jakes-MacBook-Pro.local>
2020-02-28 13:53:45 +1030

    redirects 90%

--- modules/siteSettings/deleteRedirect.js
+++ modules/siteSettings/deleteRedirect.js
@@ -1,5 +1,5 @@
 module.exports = {
   exec({ $mongo: { $db } }, { _id }) {
-    return $db.collection('categories').deleteOne({ _id });
+    return $db.collection('redirects').deleteOne({ _id });
   },
 };
