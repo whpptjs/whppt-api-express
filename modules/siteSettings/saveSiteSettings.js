3a5cddcaf1aa890f800cdddf80be2c3cf961804d
Gilly <ben@sveltestudios.com>
2020-03-05 16:01:26 +1030

    fixed saving site settings

--- modules/siteSettings/saveSiteSettings.js
+++ modules/siteSettings/saveSiteSettings.js
@@ -1,5 +1,7 @@
 module.exports = {
   exec({ $mongo: { $save } }, { siteSettings }) {
+    siteSettings._id = 'siteSettings';
+
     return $save('site', siteSettings);
     // return $db.collection('site').updateOne({ _id: 'siteSettings' }, { $set: siteSettings }, { upsert: true });
   },
