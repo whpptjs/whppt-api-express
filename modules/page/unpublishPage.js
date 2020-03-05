b0c0e11416e8d20281f934dfa92c87e3339e7911
Gilly <ben@sveltestudios.com>
2020-03-05 12:32:44 +1030

    callback features

--- modules/page/unpublishPage.js
+++ modules/page/unpublishPage.js
@@ -1,9 +1,13 @@
 const assert = require('assert');
+const { unPublishCallBack } = require(`${process.cwd()}/whppt.config.js`);
 
 module.exports = {
   exec({ $mongo: { $unpublish } }, { _id }) {
     assert(_id, 'A Page Id must be provided.');
 
-    return $unpublish('pages', _id);
+    return $unpublish('pages', _id).then(() => {
+      if (!unPublishCallBack) return page;
+      return unPublishCallBack(page).then(() => page);
+    });
   },
 };
