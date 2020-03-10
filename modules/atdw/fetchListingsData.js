04afb3538a2288e7d20d8bb4106a601ce236a589
lucas <lvcasjm@gmail.com>
2020-03-10 13:49:32 +1030

    debugging fetchListingsData

--- modules/atdw/fetchListingsData.js
+++ modules/atdw/fetchListingsData.js
@@ -20,10 +20,11 @@ module.exports = {
       .then(listings => {
         return chainPromises(listings)
           .then(() => Promise.all(map(listings, listing => updateProductServices($db, listing))))
-          .then(() => Promise.all(map(listings, listing => updateProductDetails($db, listing))));
+          .then(() => Promise.all(map(listings, listing => updateProductDetails($db, listing))))
+          .then(() => Promise.resolve({ statusCode: 200, message: 'Successfully updated Listings' }));
       })
       .catch(err => {
-        // TODO: if 404 set listing as removed in mongo
+        console.log(err);
         throw err;
       });
   },
@@ -45,24 +46,30 @@ const fetchProductDetails = ($atdw, listing) => {
   if (listing.listingType === 'service') return;
 
   const { _id } = listing;
-  return $atdw.$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${_id}`).then(productData => {
-    forEach(atdwFields, (getFieldValue, fieldKey) => {
-      const property = listing[fieldKey];
-      if (!property || property.provider !== 'atdw') return;
-      property.value = getFieldValue(productData, property.path);
-    });
+  return (
+    $atdw
+      .$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${_id}`)
+      .then(productData => {
+        forEach(atdwFields, (getFieldValue, fieldKey) => {
+          const property = listing[fieldKey];
+          if (!property || property.provider !== 'atdw') return;
+          property.value = getFieldValue(productData, property.path);
+        });
 
-    listing.atdw = { ...listing.atdw, ...productData };
-    listing.atdw.multimedia = filterMultimedia(productData.multimedia);
-    listing.hasFullATDWData = true;
-  });
+        listing.atdw = { ...listing.atdw, ...productData };
+        listing.atdw.multimedia = filterMultimedia(productData.multimedia);
+        listing.hasFullATDWData = true;
+      })
+      // TODO: if 404 set listing as removed in mongo
+      .catch(err => console.error(err))
+  );
 };
 
 const updateProductServices = ($db, listing) => {
   if (!(listing.atdw.productCategoryId === 'TOUR' && listing.atdw.services && listing.atdw.services.length)) return Promise.resolve();
-
+  console.info('updating service details for listing' + listing._id);
   return Promise.all(
-    map(listing.atdw.services, (s, index) => {
+    map(listing.atdw.services, s => {
       const service = createServiceListing(s, listing);
       return $db.collection('listings').updateOne({ _id: service._id }, { $set: service }, { upsert: true });
     })
@@ -70,7 +77,12 @@ const updateProductServices = ($db, listing) => {
 };
 
 const updateProductDetails = ($db, listing) => {
-  return $db.collection('listings').updateOne({ _id: listing._id }, { $set: listing });
+  console.info('updating details for listing ' + listing._id);
+  return $db
+    .collection('listings')
+    .updateOne({ _id: listing._id }, { $set: listing })
+    .then(() => Promise.resolve());
+  // .catch(err => console.error('update failed for ' + listing._id));
 };
 
 function createServiceListing(service, listing) {
@@ -100,186 +112,3 @@ function createServiceListing(service, listing) {
   if (_service.taggedCategories.provider === 'atdw') _service.taggedCategories.value = listing.atdwCategories.value;
   return _service;
 }
-
-// return $db
-//   .collection('listings')
-//   .find()
-//   .toArray()
-//   .then(listings => {
-//     // return Promise.all(map(listings, listing => {})); // solution for async loop
-//     listings.forEach(listing => {
-//       return $atdw
-//         .$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${listing._id}`)
-//         .then(listingData => {
-//           const { multimedia } = listingData;
-//           listingData.multimedia = filterMultimedia(multimedia);
-//
-//           const { _id } = listing;
-//
-//           return $db
-//             .collection('listings')
-//             .findOne({ _id })
-//             .then(({ atdw }) => {
-//               const updatedProduct = { ...atdw, ...listingData };
-//               return $db
-//                 .collection('listings')
-//                 .updateOne({ _id }, { $set: { atdw: updatedProduct, hasFullATDWData: true } })
-//                 .then(() => {
-//                   return Promise.resolve({ statusCode: 200, message: 'OK' });
-//                 });
-//             })
-//             .catch(err => {
-//               console.error(err.message);
-//               throw new Error(err);
-//             });
-//         })
-//         .catch(err => {
-//           console.error(`Unable to update data from ATDW for product ${listing._id}`);
-//           throw new Error(err);
-//         });
-//     });
-//   });
-
-// return $db
-//   .collection('listings')
-//   .find()
-//   .toArray()
-//   .then(listings => {
-//     // return Promise.all(map(listings, listing => {})); // solution for async loop
-//     listings.forEach(listing => {
-//       return $atdw
-//         .$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${listing._id}`)
-//         .then(listingData => {
-//           const { multimedia } = listingData;
-//           listingData.multimedia = filterMultimedia(multimedia);
-//
-//           const { _id } = listing;
-//
-//           return $db
-//             .collection('listings')
-//             .findOne({ _id })
-//             .then(({ atdw }) => {
-//               const updatedProduct = { ...atdw, ...listingData };
-//               return $db
-//                 .collection('listings')
-//                 .updateOne({ _id }, { $set: { atdw: updatedProduct, hasFullATDWData: true } })
-//                 .then(() => {
-//                   return Promise.resolve({ statusCode: 200, message: 'OK' });
-//                 });
-//             })
-//             .catch(err => {
-//               console.error(err.message);
-//               throw new Error(err);
-//             });
-//         })
-//         .catch(err => {
-//           console.error(`Unable to update data from ATDW for product ${listing._id}`);
-//           throw new Error(err);
-//         });
-//     });
-//   });
-
-// const assert = require('assert');
-// const { uniq, forEach } = require('lodash');
-// const atdwFields = require('./atdwFields');
-// const filterMultimedia = require('./filterMultimedia');
-//
-// // Whppt Config
-// const { atdw } = require(`${process.cwd()}/whppt.config.js`);
-//
-// module.exports = {
-//   exec({ $atdw, $mongo: { $db } }, { limit }) {
-//     const { apiUrl, apiKey } = atdw;
-//
-//     assert(apiUrl, 'Please provide an ATDW URL.');
-//     assert(apiKey, 'Please provide an ATDW API Key.');
-//
-//     return $db
-//       .collection('listings')
-//       .find()
-//       .limit(Number(limit))
-//       .toArray()
-//       .then(listings => {
-//         const ops = [];
-//
-//         const listingPromises = listings.map(listing => {
-//           const { _id } = listing;
-//           return $atdw.$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${_id}`).then(listingData => {
-//             forEach(atdwFields, (getFieldValue, fieldKey) => {
-//               const property = listing[fieldKey];
-//
-//               if (!property || property.provider !== 'atdw') return;
-//               property.value = getFieldValue(listingData, property.path);
-//             });
-//             const { multimedia } = listingData;
-//             listingData.multimedia = filterMultimedia(multimedia);
-//
-//             listing.taggedCategories.value = uniq([...listing.atdwCategories.value, ...listing.customCategories.value]);
-//             listing.atdw = { ...atdw, ...listingData };
-//             listing.hasFullATDWData = true;
-//
-//             ops.push({
-//               updateOne: {
-//                 filter: { _id },
-//                 update: {
-//                   $set: listing,
-//                 },
-//               },
-//             });
-//           });
-//         });
-//
-//         return Promise.all(listingPromises).then(() => {
-//           return $db
-//             .collection('listings')
-//             .bulkWrite(ops, { ordered: false })
-//             .then(() => {
-//               return Promise.resolve({ statusCode: 200, message: 'OK' });
-//             });
-//         });
-//       })
-//       .catch(err => {
-//         console.error(`Unable to update data from ATDW for product`);
-//         throw new Error(err);
-//       });
-//   },
-// };
-//
-// // return $db
-// //   .collection('listings')
-// //   .find()
-// //   .toArray()
-// //   .then(listings => {
-// //     // return Promise.all(map(listings, listing => {})); // solution for async loop
-// //     listings.forEach(listing => {
-// //       return $atdw
-// //         .$get(`https://${apiUrl}/api/atlas/product?key=${apiKey}&out=json&productId=${listing._id}`)
-// //         .then(listingData => {
-// //           const { multimedia } = listingData;
-// //           listingData.multimedia = filterMultimedia(multimedia);
-// //
-// //           const { _id } = listing;
-// //
-// //           return $db
-// //             .collection('listings')
-// //             .findOne({ _id })
-// //             .then(({ atdw }) => {
-// //               const updatedProduct = { ...atdw, ...listingData };
-// //               return $db
-// //                 .collection('listings')
-// //                 .updateOne({ _id }, { $set: { atdw: updatedProduct, hasFullATDWData: true } })
-// //                 .then(() => {
-// //                   return Promise.resolve({ statusCode: 200, message: 'OK' });
-// //                 });
-// //             })
-// //             .catch(err => {
-// //               console.error(err.message);
-// //               throw new Error(err);
-// //             });
-// //         })
-// //         .catch(err => {
-// //           console.error(`Unable to update data from ATDW for product ${listing._id}`);
-// //           throw new Error(err);
-// //         });
-// //     });
-// //   });
