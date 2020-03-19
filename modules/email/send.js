135727e2c351fd4910800133effe8adda89e56e5
Ethan <sunnysun9312@gmail.com>
2020-03-19 18:14:43 +1030

    Added default email address

--- modules/email/send.js
+++ modules/email/send.js
@@ -3,25 +3,21 @@ const EmailSyntax = require('email-syntax').EmailSyntax;
 module.exports = {
   exec({ $mongo: { $fetch, $save }, $email, $id }, { to, subject, data, content, storeAt }) {
     return $fetch('site', 'siteSettings').then(res => {
-      const localValid = false;
-      const domainValid = false;
-      try {
-        localValid = EmailSyntax.validateLocalPart(res.emailLocal);
-        domainValid = EmailSyntax.validateLocalPart(res.emailDomain);
-        if (!localValid) throw new Error("Email address's local part is invalid.");
-        if (!domainValid) throw new Error("Email address's domain name is invalid.");
-      } catch (err) {
-        throw new Error("Email address's local/domain part is invalid.");
-      }
-
-      const from = `${res.emailLocal}@${res.emailDomain}`;
-      return $email.send({ from, to, subject, html: content }).then(() => {
-        return $save(storeAt, {
-          _id: $id(),
-          subject,
-          data: data || content,
-          to,
-          sentOn: new Date(),
+      const local = res.emailLocal || 'no-reply';
+      if (!EmailSyntax.validateLocalPart(local)) throw new Error("Email address's local part is invalid.");
+      let domain = '';
+      return $email.getDomainList.then(domainList => {
+        domain = res.emailLocal || domainList[0];
+        if (!EmailSyntax.validateDomainName(domain)) throw new Error("Email address's domain name is invalid.");
+        const from = `${local}@${domain}`;
+        return $email.send({ from, to, subject, html: content }).then(() => {
+          return $save(storeAt, {
+            _id: $id(),
+            subject,
+            data: data || content,
+            to,
+            sentOn: new Date(),
+          });
         });
       });
     });
