29844846c707896db478d6d348c8933dca6b5161
Matt Martin <matt.martin0108@gmail.com>
2020-10-20 15:28:34 +1030

    aws s3 and ses code separated to fix issue where aws-sdk config is crossed

--- context/aws/index.js
+++ context/aws/index.js
@@ -1,23 +1,7 @@
-const awsSDK_S3 = require('aws-sdk');
-const awsSDK_SES = require('aws-sdk');
 const S3 = require('./s3');
 const SES = require('./ses');
 
-const S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID;
-const S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY;
-const AWS_REGION = process.env.AWS_REGION || 'ap-southeast-2';
-
-awsSDK_S3.config.update({
-  accessKeyId: S3_ACCESS_KEY_ID,
-  secretAccessKey: S3_SECRET_ACCESS_KEY,
-  region: AWS_REGION,
-});
-
-awsSDK_SES.config.update({
-  accessKeyId: SES_ACCESS_KEY_ID,
-  secretAccessKey: SES_SECRET_ACCESS_KEY,
-  region: AWS_REGION,
-});
+const { sendEmail, getDomainIdentities } = SES();
 
 const {
   uploadImage: uploadImageToS3,
@@ -27,7 +11,6 @@ const {
   fetchDoc: fetchDocFromS3,
   removeImage: removeImageFromS3,
   removeDoc: removeDocFromS3,
-} = S3(awsSDK_S3);
-const { sendEmail, getDomainIdentities } = SES(awsSDK_SES);
+} = S3();
 
 module.exports = { uploadImageToS3, fetchImageFromS3, fetchVideoFromS3, uploadDocToS3, fetchDocFromS3, removeImageFromS3, sendEmail, getDomainIdentities, removeDocFromS3 };
