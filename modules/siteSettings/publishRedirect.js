9957474e7e92b8bc4717fd05ab47b23c62414110
lucas <lvcasjm@gmail.com>
2021-01-04 12:12:25 +1030

    spacing

--- modules/siteSettings/publishRedirect.js
+++ modules/siteSettings/publishRedirect.js
@@ -2,6 +2,7 @@ module.exports = {
   exec({ $id, $mongo: { $publish, $save } }, { redirect }) {
     redirect._id = redirect._id || $id();
     redirect.published = true;
+
     return $save('redirects', redirect).then(() => {
       return $publish('redirects', redirect).then(() => {
         return redirect;
