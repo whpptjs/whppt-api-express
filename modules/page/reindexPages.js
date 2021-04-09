e0a2a61f803ac7d09e3463e256424928f5c231f3
lucas <lvcasjm@gmail.com>
2021-04-09 10:49:55 +0930

    feat: reindexPages command

---
+++ modules/page/reindexPages.js
@@ -0,0 +1,30 @@
+const save = require('./save');
+
+module.exports = {
+  authorise({ $roles }, { user }) {
+    return $roles.validate(user, [], true);
+  },
+  exec(context) {
+    const {
+      whpptOptions: { pageTypes },
+      $mongo: { $db },
+    } = context;
+
+    const collections = pageTypes.map(pt => (pt.collection ? pt.collection.name : pt.key || pt.name));
+    const pageQueries = collections.map(collection => $db.collection(collection).find().toArray());
+
+    return Promise.all(pageQueries).then(collectionPages => {
+      const saveQueries = [];
+      collectionPages.forEach(pages => {
+        pages.forEach(page => {
+          const pageType = pageTypes.find(pt => (pt.key || pt.name) === page.pageType);
+          const pageCollection = pageType.collection ? pageType.collection.name : pageType.key || pageType.name;
+
+          saveQueries.push(save.exec(context, { page, collection: pageCollection }));
+        });
+      });
+
+      return Promise.all(saveQueries).then(() => `${saveQueries.length} page(s) reindex`);
+    });
+  },
+};
