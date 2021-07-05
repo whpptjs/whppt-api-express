c80e77db7cdb68d3a9d2c2b34c39b1f954417150
Jake Vandborg <jake.vandborg@gmail.com>
2021-07-06 08:23:27 +0930

    added page slug to link dependency objects

--- utils/linksExtractor.js
+++ utils/linksExtractor.js
@@ -2,7 +2,7 @@ const { map, forEach, find, compact } = require('lodash');
 const $id = require('uniqid');
 
 module.exports = function linksExtractor(pageType, page) {
-  let links = pageType.extractLinks ? map(pageType.extractLinks(page), link => ({ _id: $id(), parentId: page._id, href: link, type: 'link' })) : [];
+  let links = pageType.extractLinks ? map(pageType.extractLinks(page), link => ({ _id: $id(), parentId: page._id, slug: page.slug, href: link, type: 'link' })) : [];
 
   const contentSections = pageType.contentSections || ['contents'];
 
@@ -11,7 +11,7 @@ module.exports = function linksExtractor(pageType, page) {
       const componentType = find(pageType.components, c => c.componentType === componentData.componentType);
 
       const componentLinks = compact(componentType && componentType.extractLinks ? componentType.extractLinks(componentData) : []);
-      const _componentLinks = map(componentLinks, cl => ({ _id: $id(), parentId: page._id, href: cl, type: 'link' }));
+      const _componentLinks = map(componentLinks, cl => ({ _id: $id(), parentId: page._id, slug: page.slug, href: cl, type: 'link' }));
 
       links = [...links, ..._componentLinks];
     });
