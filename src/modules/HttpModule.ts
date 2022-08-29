50069a12f144d1637612c39afb7287a2b579d9bd
leandrosveltestudios <leandro@sveltestudios.com>
2022-08-29 15:57:50 +1000

    added req parameter to HttpModule

--- src/modules/HttpModule.ts
+++ src/modules/HttpModule.ts
@@ -1,6 +1,6 @@
 import { ContextType } from '../context/Context';
 
 export type HttpModule<ArgType, ReturnType = ArgType> = {
-  authorise: (context: ContextType, args: ArgType & { user: any }) => Promise<any>;
-  exec: (context: ContextType, args: ArgType) => Promise<ReturnType>;
+  authorise: (context: ContextType, args: ArgType & { user: any }, req?: any) => Promise<any>;
+  exec: (context: ContextType, args: ArgType, req?: any) => Promise<ReturnType>;
 };
