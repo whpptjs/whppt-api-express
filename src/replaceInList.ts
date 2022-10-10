b92d9db9bb1bd645f70a769409f1f7000a2ccaea
Ben_G <ben@sveltestudios.com>
2022-10-11 10:27:39 +1030

    Fixed linter issue

--- src/replaceInList.ts
+++ src/replaceInList.ts
@@ -7,10 +7,7 @@ export const replaceInList = function <T extends object>(
     throw new Error(`New item missing property ${id.toString()}`);
 
   const oldItem = items.find(i => i[id] === item[id]);
-  if (!oldItem)
-    throw new Error(
-      `Original item with ${id.toString()} = ${item[id].toString()} not found`
-    );
+  if (!oldItem) throw new Error(`Original item with ${id.toString()}} not found`);
 
   const oldItemIndex = items.indexOf(oldItem);
   const newItems = [...items];
