4cf135fd228f8376dc60942fd9822230eca9a2c8
Ben_G <ben@sveltestudios.com>
2022-10-10 16:52:25 +1030

    Linter

--- src/replaceInList.ts
+++ src/replaceInList.ts
@@ -1,8 +1,16 @@
-export const replaceInList = function <T extends object>(items: T[], item: T, id: keyof T = '_id' as keyof T): T[] {
-  if (!Object.keys(item).find(k => k === id)) throw new Error(`New item missing property ${id.toString()}`);
+export const replaceInList = function <T extends object>(
+  items: T[],
+  item: T,
+  id: keyof T = '_id' as keyof T
+): T[] {
+  if (!Object.keys(item).find(k => k === id))
+    throw new Error(`New item missing property ${id.toString()}`);
 
   const oldItem = items.find(i => i[id] === item[id]);
-  if (!oldItem) throw new Error(`Original item with ${id.toString()} = ${item[id].toString()} not found`);
+  if (!oldItem)
+    throw new Error(
+      `Original item with ${id.toString()} = ${item[id].toString()} not found`
+    );
 
   const oldItemIndex = items.indexOf(oldItem);
   const newItems = [...items];
