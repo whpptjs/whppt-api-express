ec679a42e64b0eb02aa32fc21ee3705bc9c80066
Ben_G <ben@sveltestudios.com>
2022-11-24 12:34:03 +1030

    testiong

--- src/modules/member/Secure.ts
+++ src/modules/member/Secure.ts
@@ -8,25 +8,39 @@ export type LoggedInMemberInfo = {
   name: string;
 };
 
-type ParseMemberTokenFromCookie = (cookies: any) => LoggedInMemberInfo;
-const parseMemberTokenFromCookie: ParseMemberTokenFromCookie = memberauthtoken => {
+type ParseMemberTokenFromCookie = (cookies: any, appKey: string) => LoggedInMemberInfo;
+const parseMemberTokenFromCookie: ParseMemberTokenFromCookie = (
+  memberauthtoken,
+  appKey
+) => {
   const token = memberauthtoken;
-  var decoded = jwt.verify(token, process.env.APP_KEY || '');
+  var decoded = jwt.verify(token, appKey);
+
   return decoded as LoggedInMemberInfo;
 };
 
 export const Secure: SecureModule = module => {
   const secureModule: HttpModule<any, any> = {
-    authorise: (context: any, args: any, req: any) => {
-      if (!module) return Promise.reject({ status: 404, message: 'Module not found' });
-      const member = parseMemberTokenFromCookie(req.headers.memberauthtoken);
-      if (!member) return Promise.reject({ status: 404, message: 'Member not found' });
-      if (!module.authorise) return Promise.resolve();
-      return module.authorise({ ...context, member }, args, req);
+    authorise: (context, args: any, req: any) => {
+      return context.$hosting.then(config => {
+        if (!module) return Promise.reject({ status: 404, message: 'Module not found' });
+        const member = parseMemberTokenFromCookie(
+          req.headers.memberauthtoken,
+          config.security.appKey
+        );
+        if (!member) return Promise.reject({ status: 404, message: 'Member not found' });
+        if (!module.authorise) return Promise.resolve();
+        return module.authorise({ ...context, member }, args, req);
+      });
     },
-    exec: (context: any, args: any, req: any) => {
-      const member = parseMemberTokenFromCookie(req.headers.memberauthtoken);
-      return module.exec({ ...context, member }, args, req);
+    exec: (context, args: any, req: any) => {
+      return context.$hosting.then(config => {
+        const member = parseMemberTokenFromCookie(
+          req.headers.memberauthtoken,
+          config.security.appKey
+        );
+        return module.exec({ ...context, member }, args, req);
+      });
     },
   };
   return secureModule;
