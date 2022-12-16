ab75e84750a48abd540ec8faf8dedd798e574333
Leandro Lavore <leandrolavore@192-168-1-110.tpgi.com.au>
2022-12-16 15:21:28 +1000

    added pagination ond total for list member's orders

--- src/modules/member/listPreviousOrders.ts
+++ src/modules/member/listPreviousOrders.ts
@@ -2,14 +2,24 @@ import assert from 'assert';
 import { HttpModule } from '../HttpModule';
 import { Order } from '../order/Models/Order';
 
-const listPreviousOrders: HttpModule<{ memberId: string }, Order[]> = {
-  exec({ $database }, { memberId }) {
+const listPreviousOrders: HttpModule<{ memberId: string, currentPage: string; limit: string },{ orders: Order[], total: number}> = {
+  exec({ $database }, { memberId, currentPage, limit }) {
     assert(memberId, 'A memberId is required');
+    const numLimit = Number(limit);
+    const numCurrentPage = Number(currentPage);
+    const query = { memberId, checkoutStatus: { $ne: 'pending' } };
 
-    return $database.then(({ queryDocuments }) => {
-      return queryDocuments<Order>('orders', {
-        filter: { memberId, checkoutStatus: { $ne: 'pending' } },
-        sort: { updatedAt: -1 },
+    return $database.then(({ queryDocuments,countDocuments }) => {
+      return Promise.all([
+        queryDocuments<Order>('orders', {
+          filter: query,
+          sort: { updatedAt: -1 },
+          limit: numLimit,
+          skip: numLimit * (numCurrentPage - 1),
+        }),
+        countDocuments('orders', { filter: query }),
+      ]).then(([orders, total]) => {
+        return { orders, total };
       });
     });
   },
