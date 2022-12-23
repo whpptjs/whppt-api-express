61e6a69cb46d2f653c9400a53479bf61e28e78ba
Ben_G <ben@sveltestudios.com>
2022-12-23 16:28:17 +1030

    loading membership fix

--- src/modules/membershipTier/load.ts
+++ src/modules/membershipTier/load.ts
@@ -1,11 +1,13 @@
 import { HttpModule } from '../HttpModule';
 import { MembershipOptions } from './Models/MembershipTier';
 
-const load: HttpModule<{ domainId: string }, MembershipOptions> = {
+const load: HttpModule<{ domainId: string }, MembershipOptions | {}> = {
   exec({ $database }, { domainId }) {
     return $database.then(({ document }) => {
       return document
-        .fetch<MembershipOptions>('site', `membershipOptions_${domainId}`)
+        .query<MembershipOptions>('site', {
+          filter: { _id: `membershipOptions_${domainId}` },
+        })
         .then(membershipOptions => {
           return membershipOptions || {};
         });
