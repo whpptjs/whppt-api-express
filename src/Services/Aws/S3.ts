b5f3c094f73d8eb2c6e38a6f5e38074d1d06a06b
Ben_G <ben@sveltestudios.com>
2023-01-10 18:04:34 +1030

    Error handleing for images

--- src/Services/Aws/S3.ts
+++ src/Services/Aws/S3.ts
@@ -51,7 +51,10 @@ export const S3: S3Constructor = $hosting => {
             Metadata: meta,
           })
         )
-        .then(() => {});
+        .then(() => {})
+        .catch(err => {
+          throw new Error(err?.message || 'Uploading image failed.');
+        });
     });
   };
 
@@ -68,7 +71,10 @@ export const S3: S3Constructor = $hosting => {
             Key: path,
           })
         )
-        .then(() => {});
+        .then(() => {})
+        .catch(err => {
+          throw new Error(err?.message || 'Removing image failed.');
+        });
     });
   };
 
@@ -98,6 +104,9 @@ export const S3: S3Constructor = $hosting => {
         .then(fileData => {
           if (!fileData || !fileData.Body) throw new Error('No file body');
           return streamToBuffer(fileData.Body);
+        })
+        .catch(err => {
+          throw new Error(err?.message || 'Fetching image failed.');
         });
     });
   };
