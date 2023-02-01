72221a1a86ab08abd10050cbc66a0a7ad6b8c6b8
Ben_G <ben@sveltestudios.com>
2023-02-01 15:08:25 +1030

    AusPost Label when changed will create an event

--- src/modules/order/createAuspostLabel.ts
+++ src/modules/order/createAuspostLabel.ts
@@ -75,12 +75,52 @@ const createAuspostLabel: HttpModule<
                 }),
               ];
               assert(loadedOrder?.shipping, 'Order shipping is required');
-
               loadedOrder.shipping.ausPost = {
                 shipmentId,
                 label_request_id,
                 status: 'labelPrinted',
               };
+
+              const _shipping = {
+                contactDetails: {
+                  firstName: firstName || loadedOrder?.shipping?.contactDetails.firstName,
+                  lastName: lastName || loadedOrder?.shipping?.contactDetails.lastName,
+                  company: loadedOrder?.shipping?.contactDetails?.company,
+                },
+                address: {
+                  number: number || loadedOrder?.shipping?.address?.number,
+                  street: street || loadedOrder?.shipping?.address?.street,
+                  suburb: suburb || loadedOrder?.shipping?.address?.suburb,
+                  postCode: postCode || loadedOrder?.shipping?.address?.postCode,
+                  city: loadedOrder?.shipping?.address?.city,
+                  state: state || loadedOrder?.shipping?.address?.state,
+                  country: loadedOrder?.shipping?.address?.country,
+                },
+              };
+
+              if (
+                checkShippingAddressChanged(
+                  _shipping.address,
+                  loadedOrder.shipping.address
+                ) ||
+                checkShippingDetailsChanged(
+                  _shipping.contactDetails,
+                  loadedOrder.shipping.contactDetails
+                )
+              ) {
+                events.push(
+                  createEvent('OrderShippingDetailsUpdatedByStaff', {
+                    _id: orderId,
+                    shipping: _shipping,
+                  })
+                );
+                loadedOrder.shipping = {
+                  ...loadedOrder.shipping,
+                  address: _shipping.address || {},
+                  contactDetails: _shipping.contactDetails,
+                };
+              }
+
               return document.saveWithEvents('orders', loadedOrder, events, {
                 session,
               });
@@ -93,3 +133,40 @@ const createAuspostLabel: HttpModule<
 };
 
 export default Secure(createAuspostLabel);
+
+const checkShippingAddressChanged = (
+  newAddress: {
+    number: any;
+    street: any;
+    suburb: any;
+    postCode: any;
+    city?: string;
+    state: any;
+    country?: string;
+  },
+  oldAddress: {
+    number: any;
+    street: any;
+    suburb: any;
+    postCode: any;
+    city?: string;
+    state: any;
+    country?: string;
+  }
+) => {
+  if (newAddress.number !== oldAddress.number) return true;
+  if (newAddress.street !== oldAddress.street) return true;
+  if (newAddress.suburb !== oldAddress.suburb) return true;
+  if (newAddress.postCode !== oldAddress.postCode) return true;
+  if (newAddress.state !== oldAddress.state) return true;
+  return false;
+};
+const checkShippingDetailsChanged = (
+  newContactDetails: { firstName: any; lastName: any; company: any },
+  oldContactDetails: { firstName: any; lastName: any; company: any }
+) => {
+  if (newContactDetails.firstName !== oldContactDetails.firstName) return true;
+  if (newContactDetails.lastName !== oldContactDetails.lastName) return true;
+  if (newContactDetails.company !== oldContactDetails.company) return true;
+  return false;
+};
