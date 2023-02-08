a634cac2ab5b7dae64a5bd931b38d6dd1896bc1d
Leandro <leandro@sveltestudios.com>
2023-02-08 13:46:53 +1000

    checking staff credentials before refund authorized

--- src/modules/order/refund.ts
+++ src/modules/order/refund.ts
@@ -3,65 +3,90 @@ import { assign } from 'lodash';
 import { HttpModule } from '../HttpModule';
 import { Secure } from '../staff/Secure';
 import { Order } from './Models/Order';
+import { findActiveStaff } from '../staff/login';
 
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
-const refund: HttpModule<{ orderId: string; refundReason: string }, void> = {
-  exec(context, { orderId, refundReason }) {
+const refund: HttpModule<
+  { orderId: string; refundReason: string; username: string; password: string },
+  void
+> = {
+  exec(context, { orderId, refundReason, username, password }) {
     assert(orderId, 'Order Id not found');
     assert(refundReason, 'A Reason not provided');
-    return context.$database.then(({ document, startTransaction }) => {
-      return document.fetch<Order>('orders', orderId).then(loadedOrder => {
-        assert(loadedOrder, 'Order not found');
-        assert(loadedOrder?.stripe?.status === 'paid', 'Order not in a paid status');
-        const amount =
-          (loadedOrder?.payment?.subTotal || 0) -
-          (loadedOrder?.payment?.memberTotalDiscount || 0);
-        assign(loadedOrder, {
-          ...loadedOrder,
-          checkoutStatus: 'refunded',
-          stripe: {
-            ...loadedOrder.stripe,
-            status: 'refunded',
-            refund: {
-              reason: refundReason,
-              amount,
-              by: {
-                username: context.staff.sub.username,
-                _id: context.staff.sub._id,
-              },
-            },
-          },
-          payment: {
-            ...loadedOrder.payment,
-            status: 'refunded',
-            refund: {
-              amount,
-              reason: refundReason,
-              by: {
-                username: context.staff.sub.username,
-                _id: context.staff.sub._id,
-              },
-            },
-          },
-        });
+    assert(username, 'A username or email address is required.');
+    assert(password, 'A password is required.');
 
-        const events = [
-          context.createEvent('OrderPaymentRefundedThroughStripe', {
-            _id: orderId,
-            refundReason,
-          }),
-        ];
+    return context.$database.then(db => {
+      return findActiveStaff(db, username).then(staffMember => {
+        return context.$security.encrypt(password).then(() => {
+          return context.$security
+            .compare(password, staffMember?.password || '')
+            .then(passwordMatches => {
+              if (!passwordMatches)
+                return Promise.reject(
+                  new Error("The password that you've entered is incorrect.")
+                );
 
-        return startTransaction(session => {
-          return stripe.refunds
-            .create({
-              payment_intent: loadedOrder?.stripe?.intentId,
-              amount: Math.round(amount),
-            })
-            .then(() => {
-              return document.saveWithEvents('orders', loadedOrder, events, {
-                session,
+              return context.$database.then(({ document, startTransaction }) => {
+                return document.fetch<Order>('orders', orderId).then(loadedOrder => {
+                  assert(loadedOrder, 'Order not found');
+                  assert(
+                    loadedOrder?.stripe?.status === 'paid',
+                    'Order not in a paid status'
+                  );
+                  const amount =
+                    (loadedOrder?.payment?.subTotal || 0) -
+                    (loadedOrder?.payment?.memberTotalDiscount || 0);
+                  assign(loadedOrder, {
+                    ...loadedOrder,
+                    checkoutStatus: 'refunded',
+                    stripe: {
+                      ...loadedOrder.stripe,
+                      status: 'refunded',
+                      refund: {
+                        reason: refundReason,
+                        amount,
+                        by: {
+                          username: context.staff.sub.username,
+                          _id: context.staff.sub._id,
+                        },
+                      },
+                    },
+                    payment: {
+                      ...loadedOrder.payment,
+                      status: 'refunded',
+                      refund: {
+                        amount,
+                        reason: refundReason,
+                        by: {
+                          username: context.staff.sub.username,
+                          _id: context.staff.sub._id,
+                        },
+                      },
+                    },
+                  });
+
+                  const events = [
+                    context.createEvent('OrderPaymentRefundedThroughStripe', {
+                      _id: orderId,
+                      refundReason,
+                    }),
+                  ];
+
+                  return startTransaction(session => {
+                    return stripe.refunds
+                      .create({
+                        payment_intent: loadedOrder?.stripe?.intentId,
+                        amount: Math.round(amount),
+                      })
+                      .then(() => {
+                        return document.saveWithEvents('orders', loadedOrder, events, {
+                          session,
+                        });
+                      });
+                  });
+                });
               });
             });
         });
--- src/modules/order/refundViaCash.ts
+++ src/modules/order/refundViaCash.ts
@@ -3,47 +3,75 @@ import { assign } from 'lodash';
 import { HttpModule } from '../HttpModule';
 import { Secure } from '../staff/Secure';
 import { Order } from './Models/Order';
+import { findActiveStaff } from '../staff/login';
 
-const refund: HttpModule<{ orderId: string; refundReason: string }, void> = {
-  exec(context, { orderId, refundReason }) {
+const refund: HttpModule<
+  { orderId: string; refundReason: string; username: string; password: string },
+  void
+> = {
+  exec(context, { orderId, refundReason, username, password }) {
     assert(orderId, 'Order Id not found');
     assert(refundReason, 'A Reason not provided');
-    return context.$database.then(({ document, startTransaction }) => {
-      return document.fetch<Order>('orders', orderId).then(loadedOrder => {
-        assert(loadedOrder, 'Order not found');
-        assert(loadedOrder?.payment?.status === 'paid', 'Order not in a paid status');
-        assert(loadedOrder?.checkoutStatus === 'paid', 'Order not in a paid status');
-        const amount =
-          (loadedOrder?.payment?.subTotal || 0) -
-          (loadedOrder?.payment?.memberTotalDiscount || 0);
-        assign(loadedOrder, {
-          ...loadedOrder,
-          checkoutStatus: 'refunded',
-          payment: {
-            ...loadedOrder.payment,
-            status: 'refunded',
-            refund: {
-              amount,
-              reason: refundReason,
-              by: {
-                username: context.staff.sub.username,
-                _id: context.staff.sub._id,
-              },
-            },
-          },
-        });
+    assert(username, 'A username or email address is required.');
+    assert(password, 'A password is required.');
 
-        const events = [
-          context.createEvent('OrderPaymentRefunded', {
-            _id: orderId,
-            refundReason,
-          }),
-        ];
+    return context.$database.then(db => {
+      return findActiveStaff(db, username).then(staffMember => {
+        return context.$security.encrypt(password).then(() => {
+          return context.$security
+            .compare(password, staffMember?.password || '')
+            .then(passwordMatches => {
+              if (!passwordMatches)
+                return Promise.reject(
+                  new Error("The password that you've entered is incorrect.")
+                );
 
-        return startTransaction(session => {
-          return document.saveWithEvents('orders', loadedOrder, events, {
-            session,
-          });
+              return context.$database.then(({ document, startTransaction }) => {
+                return document.fetch<Order>('orders', orderId).then(loadedOrder => {
+                  assert(loadedOrder, 'Order not found');
+                  assert(
+                    loadedOrder?.payment?.status === 'paid',
+                    'Order not in a paid status'
+                  );
+                  assert(
+                    loadedOrder?.checkoutStatus === 'paid',
+                    'Order not in a paid status'
+                  );
+                  const amount =
+                    (loadedOrder?.payment?.subTotal || 0) -
+                    (loadedOrder?.payment?.memberTotalDiscount || 0);
+                  assign(loadedOrder, {
+                    ...loadedOrder,
+                    checkoutStatus: 'refunded',
+                    payment: {
+                      ...loadedOrder.payment,
+                      status: 'refunded',
+                      refund: {
+                        amount,
+                        reason: refundReason,
+                        by: {
+                          username: context.staff.sub.username,
+                          _id: context.staff.sub._id,
+                        },
+                      },
+                    },
+                  });
+
+                  const events = [
+                    context.createEvent('OrderPaymentRefunded', {
+                      _id: orderId,
+                      refundReason,
+                    }),
+                  ];
+
+                  return startTransaction(session => {
+                    return document.saveWithEvents('orders', loadedOrder, events, {
+                      session,
+                    });
+                  });
+                });
+              });
+            });
         });
       });
     });
