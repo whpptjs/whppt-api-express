a011d6db650b9d79c93b0058b2cfb780ad72dcae
Leandro <leandro@sveltestudios.com>
2023-02-14 09:33:53 +1000

    pdfmake

---
+++ src/modules/pdf/pdfHeader.ts
@@ -0,0 +1,6 @@
+const header = [
+  { text: 'Tables', style: 'header' },
+  'Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.',
+];
+
+export default header;
