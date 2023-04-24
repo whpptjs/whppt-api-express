5ce1f8a8792307d722509d1326578b43e2c541dd
Ben_G <ben@sveltestudios.com>
2023-04-24 21:24:28 +0930

    Fixing order number search

--- src/modules/staff/listOrders.ts
+++ src/modules/staff/listOrders.ts
@@ -44,7 +44,7 @@ const listOrders: HttpModule<
             },
             {
               orderNumber: {
-                $regex: search,
+                $regex: Number(search),
               },
             },
             {
