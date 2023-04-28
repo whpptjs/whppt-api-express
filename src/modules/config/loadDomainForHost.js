34cd8003a3e589f770f7bbf772f9699773aaddbe
Ben_G <ben@sveltestudios.com>
2023-04-28 14:58:07 +0930

    Logs

--- src/modules/config/loadDomainForHost.js
+++ src/modules/config/loadDomainForHost.js
@@ -1,9 +1,11 @@
 module.exports = {
   exec({ $mongo: { $db } }, { hostname }) {
+    console.log('🚀 DOMAIN hostname Looking up', hostname);
     return $db
       .collection('domains')
       .findOne({ hostNames: hostname })
       .then(result => {
+        console.log('🚀 DOMAIN hostname Result', result);
         return result;
       });
   },
