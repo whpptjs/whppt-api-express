1daecc3160a685e561c2376812939f9581525911
Ben_G <ben@sveltestudios.com>
2023-11-08 14:53:22 +1030

    Removed logs

--- src/modules/order/findOrderForSession.ts
+++ src/modules/order/findOrderForSession.ts
@@ -1,7 +1,9 @@
+import { WhpptMongoDatabase } from 'src/Services/Database/Mongo/Database';
 import { HttpModule } from '../HttpModule';
 
 import { Order } from './Models/Order';
 import { loadOrderWithProducts } from './Queries/loadOrderWithProducts';
+import { ContextType } from 'src/context/Context';
 
 const findOrderForSession: HttpModule<
   { orderId?: string; memberId?: string },
@@ -11,28 +13,53 @@ const findOrderForSession: HttpModule<
     return $roles.validate(user, []);
   },
   exec(context, { orderId, memberId }) {
+    if (!orderId && !memberId) return Promise.resolve({});
+    return findOrder(context, orderId, memberId).then(orderIdToQuery => {
+      if (!orderIdToQuery) return {};
+
+      return loadOrderWithProducts(context, { _id: orderIdToQuery }).catch(err => {
+        if (err.status === 404) return {};
+        throw err;
+      });
+    });
+  },
+};
+
+type FindOrder = (
+  context: ContextType,
+  orderId?: string,
+  memberId?: string
+) => Promise<string | undefined>;
+
+const findOrder: FindOrder = (context, orderId, memberId) => {
+  return context.$database.then(database => {
+    const { document, db } = database as WhpptMongoDatabase;
+
     const _idQuery = { _id: orderId };
     const _memberIdQuery = {
+      $or: [{ fromPos: { $exists: false } }, { fromPos: { $eq: false } }],
       checkoutStatus: 'pending',
-      fromPos: { $or: [{ $exists: false }, { $eq: false }] },
       memberId,
     };
 
-    const query =
-      orderId && memberId
-        ? { $or: [_idQuery, _memberIdQuery] }
-        : orderId
-        ? _idQuery
-        : memberId
-        ? _memberIdQuery
-        : undefined;
+    return Promise.all([
+      document.query<Order>('orders', { filter: _idQuery }),
+      db.collection('orders').find(_memberIdQuery).sort({ updatedAt: -1 }).toArray(),
+    ]).then(([mainOrder, memberOrders]) => {
+      console.log(
+        '🚀 ~ file: findOrderForSession.ts:49 ~ ]).then ~ mainOrder:',
+        mainOrder?._id
+      );
+      const _memberOrder = memberOrders.length && (memberOrders[0] as any);
+      console.log(
+        '🚀 ~ file: findOrderForSession.ts:71 ~ ]).then ~ _memberOrder:',
+        _memberOrder?._id
+      );
 
-    if (!orderId && !memberId) return Promise.resolve({});
-    return loadOrderWithProducts(context, query).catch(err => {
-      if (err.status === 404) return {};
-      throw err;
+      return mainOrder ? orderId : _memberOrder ? _memberOrder?._id : undefined;
     });
-  },
+    // return document.query<Order>('orders', { filter: matchQuery }).then(order => {
+  });
 };
 
 export default findOrderForSession;
