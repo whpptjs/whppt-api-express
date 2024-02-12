7b6ce3f028a86c5ca7ca7811fe2e6bd9e0474878
NickPxF <nick.bian@pixelforce.com.au>
2024-02-12 11:22:44 +1030

    fix format

--- src/modules/member/deleteNote.ts
+++ src/modules/member/deleteNote.ts
@@ -9,28 +9,25 @@ const deleteNote: HttpModule<{ memberId: string; noteId: string }, Member> = {
     assert(memberId, 'A contact Id is required');
 
     return $database.then(database => {
-      const { document, db/*, startTransaction*/ } = database as WhpptMongoDatabase;
+      const { document, db /*, startTransaction*/ } = database as WhpptMongoDatabase;
 
       return document.fetch<Member>('members', memberId).then(loadedMember => {
         assert(loadedMember, 'Could not find member.');
 
         const recordId = loadedMember._id; // Assuming _id is a property of the Member interface
         const noteToRemoveId = noteId; // Replace with the actual _id of the note to remove
-      
-        return db.collection('members').updateOne(
-                  { _id: recordId },
-                  { $pull: { notes: { _id: noteToRemoveId } } }
-                )
-        .then(result => {
-          if (result.modifiedCount === 1) {
-            console.log(`Note with _id ${noteToRemoveId} removed successfully.`);
-          } else {
-            console.log(`Note with _id ${noteToRemoveId} not found.`);
-          }
+        return db
+          .collection('members')
+          .updateOne({ _id: recordId }, { $pull: { notes: { _id: noteToRemoveId } } })
+          .then(result => {
+            if (result.modifiedCount === 1) {
+              console.log(`Note with _id ${noteToRemoveId} removed successfully.`);
+            } else {
+              console.log(`Note with _id ${noteToRemoveId} not found.`);
+            }
 
-          return loadedMember;
-        });
-
+            return loadedMember;
+          });
       });
     });
   },
